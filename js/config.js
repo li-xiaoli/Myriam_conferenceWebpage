43f892e0a4930c7b05c0277de771f2be5f1527f8
li-xiaoli <li.xiaoli@aliyun.com>
2015-04-17 13:08:00 +0200

    min-width

--- js/config.js
+++ js/config.js
@@ -1,8 +1,4 @@
-/*
-	Miniport 2.0 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 window._skel_config = {
 	preset: 'standard',
@@ -46,3 +42,7 @@ jQuery(function() {
 		});
 
 });
+
+
+
+
